dcfddb511c79889cb3fc2694baf62d861a14be52
Lim Dong Hyeon <ilhai6@naver.com>
2024-08-09 17:33:46 +0900

    위경도 격자 변환 함수 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import { getLocal } from '@/apis/local';
+import { dfs_xy_conv } from '@/utils/xy-conversion';
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+    });
+  }, []);
   return <div>메인</div>;
 }
---
+++ utils/xy-conversion.ts
@@ -0,0 +1,52 @@
+// dfs_xy_conv.ts
+
+// 지구 반경(km), 격자 간격(km), 투영 위도, 경도 및 기준점 설정
+const RE = 6371.00877;
+const GRID = 5.0;
+const SLAT1 = 30.0;
+const SLAT2 = 60.0;
+const OLON = 126.0;
+const OLAT = 38.0;
+const XO = 43;
+const YO = 136;
+
+const DEGRAD = Math.PI / 180.0;
+
+interface Coordinates {
+  lat?: number;
+  lng?: number;
+  x?: number;
+  y?: number;
+}
+
+export function dfs_xy_conv(v1: number, v2: number): Coordinates {
+  const re = RE / GRID;
+  const slat1 = SLAT1 * DEGRAD;
+  const slat2 = SLAT2 * DEGRAD;
+  const olon = OLON * DEGRAD;
+  const olat = OLAT * DEGRAD;
+
+  let sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+  sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+
+  let sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+  sf = (Math.pow(sf, sn) * Math.cos(slat1)) / sn;
+
+  let ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+  ro = (re * sf) / Math.pow(ro, sn);
+
+  let rs: Coordinates = {};
+
+  const ra = Math.tan(Math.PI * 0.25 + v1 * DEGRAD * 0.5);
+  const computedRa = (re * sf) / Math.pow(ra, sn);
+
+  let theta = v2 * DEGRAD - olon;
+  if (theta > Math.PI) theta -= 2.0 * Math.PI;
+  if (theta < -Math.PI) theta += 2.0 * Math.PI;
+  theta *= sn;
+
+  rs.x = Math.floor(computedRa * Math.sin(theta) + XO + 0.5);
+  rs.y = Math.floor(ro - computedRa * Math.cos(theta) + YO + 0.5);
+
+  return rs;
+}
