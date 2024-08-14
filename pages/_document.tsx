96d00052a3b7a0a90d78281075b7f315c1cfeba2
Lim Dong Hyeon <ilhai6@naver.com>
2024-08-14 18:53:06 +0900

    카카오 맵 렌더링 1번으로 줄임

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <script
           type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=clusterer`}
         ></script>
       </Head>
       <body>
