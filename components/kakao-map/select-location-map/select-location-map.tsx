67aa10013539ac1883ecd1b6feb394b9b26373d7
Lim Dong Hyeon <ilhai6@naver.com>
2024-08-28 15:08:57 +0900

    검은 화면 클릭시 사라지기, 지도 클릭시 마커 생성

--- components/kakao-map/select-location-map/select-location-map.tsx
+++ components/kakao-map/select-location-map/select-location-map.tsx
@@ -1,13 +1,16 @@
 import { Location } from '@/types/local';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function SelectLocationMap({ location }: Location) {
+  const selectLocationMapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<any>(null);
+  const [visible, setVisible] = useState(true);
+  const [marker, setMarker] = useState<any>(null);
 
   useEffect(() => {
     if (!window.kakao) return;
 
-    const container = document.getElementById('selectMap');
+    const container = selectLocationMapRef.current;
     const options = {
       center: new window.kakao.maps.LatLng(location.lat, location.lng),
       level: 15,
@@ -16,17 +19,49 @@ export default function SelectLocationMap({ location }: Location) {
     };
     const newMap = new window.kakao.maps.Map(container, options);
     setMap(newMap);
+
+    const markerPosition = new window.kakao.maps.LatLng(location.lat, location.lng);
+
+    const newMarker = new window.kakao.maps.Marker({
+      position: markerPosition,
+      map: newMap,
+    });
+    setMarker(newMarker);
   }, []);
 
   useEffect(() => {
     if (map && location) {
       const moveLatLon = new window.kakao.maps.LatLng(location.lat, location.lng);
       map.setCenter(moveLatLon);
+
+      marker.setPosition(moveLatLon);
+
+      const clickListener = window.kakao.maps.event.addListener(map, 'click', (mouseEvent: any) => {
+        const latlng = mouseEvent.latLng;
+        marker.setPosition(latlng);
+      });
+
+      return () => {
+        window.kakao.maps.event.removeListener(map, 'click', clickListener);
+      };
     }
   }, [location, map]);
   return (
-    <div className="h-full">
-      <div id="selectMap" style={{ width: '100%', height: '100%' }} />
+    <div className="w-1/4 h-52 bg-slate-300 relative">
+      <div className="h-full">
+        <div ref={selectLocationMapRef} style={{ width: '100%', height: '100%' }} />
+      </div>
+      {visible && (
+        <button
+          type="button"
+          onClick={() => setVisible(false)}
+          className="z-10 flex justify-center items-center w-full h-full absolute left-0 top-0 bg-black bg-opacity-70"
+        >
+          <p className="text-white text-center">
+            지도를 클릭해서 위치를 <br /> 설정해보세요!
+          </p>
+        </button>
+      )}
     </div>
   );
 }
