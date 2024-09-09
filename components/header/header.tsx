551a82532055b378cf7059279640c03eefccd661
Lim Dong Hyeon <ilhai6@naver.com>
2024-09-09 14:40:33 +0900

    헤더 로고 링크로 변경

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,14 @@
 import LogoSvg from '@/public/images/logo.svg';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="shadow-md">
       <div className="layout-container">
         <h1>
-          <LogoSvg width="150" viewBox="0 0 200 75" />
+          <Link href={'/'}>
+            <LogoSvg width="150" viewBox="0 0 200 75" />
+          </Link>
         </h1>
       </div>
     </header>
