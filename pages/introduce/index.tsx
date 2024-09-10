242a7a5dc2e53472386f14113ad1c5d8ca245bce
Lim Dong Hyeon <ilhai6@naver.com>
2024-09-10 14:37:44 +0900

    메인 페이지 이동 버튼 추가

--- pages/introduce/index.tsx
+++ pages/introduce/index.tsx
@@ -1,10 +1,11 @@
 import NewsIntroduce from '@/components/introduce/news-introduce/news-introduce';
 import SafetyCenterIntroduce from '@/components/introduce/safety-center-introduce/safety-center-introduce';
 import WeatherIntroduce from '@/components/introduce/weather-introduce/weather-introduce';
+import Link from 'next/link';
 
 export default function IntroducePage() {
   return (
-    <main className="layout-container pb-14">
+    <main className="layout-container">
       <section className="py-14">
         <h2 className="text-2xl font-bold text-center">
           <span className="inline-block pb-4 border-b-4">홈페이지 소개</span>
@@ -17,6 +18,11 @@ export default function IntroducePage() {
           <NewsIntroduce />
         </div>
       </div>
+      <section className="flex-center py-14">
+        <Link className="flex-center bg-slate-900 text-white py-3 px-6" href={'/'}>
+          메인페이지로 이동
+        </Link>
+      </section>
     </main>
   );
 }
