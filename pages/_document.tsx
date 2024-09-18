406255791ec1cb410b37ac9ee2cccbf2c33cf753
Lim Dong Hyeon <ilhai6@naver.com>
2024-09-18 14:21:44 +0900

    메타 태그 오타 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <meta name="google-site-verification" content="ePj7IAkccvdg1YLE8puYXJ7cQdcTALN88DEDokItGew" />
         <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-        <meta name="description" content="사용자 주변의 날씨와 지킴이집, 범죄 기사들을 제공하는 사이트 압니다." />
+        <meta name="description" content="사용자 주변의 날씨와 지킴이집, 범죄 기사들을 제공하는 사이트 입니다." />
         <meta name="keywords" content="날씨, 지킴이집, 범죄, 여성, 안전, 날조아, NARJOA, narjoa" />
         <Script
           type="text/javascript"
