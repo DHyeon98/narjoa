f7e5b21bb040f4f0f50742f6eeeb495824c3b26d
LimDong <ilhai6@naver.com>
2024-09-30 00:13:06 +0900

    hydration test

--- components/weather/weather-card/weather-card.tsx
+++ components/weather/weather-card/weather-card.tsx
@@ -3,6 +3,7 @@ import WeatherDateInformation from './weather-date-information/weather-date-info
 import WeatherTodayInformation from './weather-today-information/weather-today-information';
 import { Location } from '@/types/local';
 import { DailyType } from '@/types/weather/daily';
+import { useState } from 'react';
 
 /**
  * 날씨 데이터를 시각적으로 보여주는 컴포넌트 입니다.
